41f484b6857e3099cd94c71cbb4380d511b4a4b5
Kevin <kevin@cbqasolutions.com>
2022-02-18 01:51:08 -0500

    revert iteraction browser

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,18 @@ import * as playwright from 'playwright';
 async function main(){
     const {chromium,webkit,firefox} = playwright
     //Launch browser
-    for (const browserType of [chromium,webkit,firefox]) {
-        const browser = await playwright.chromium.launch({
-            headless: false //executa in visual browser
-        });
-        //Create a context
-        const context = await browser.newContext()
-        //Create a page
-        const page = await context.newPage()
-    
-        //Carry out actions
-        await page.goto('http://whatsmyuseragent.org/');
-        await page.screenshot({path:`output/${browserType.name()}.png`})
-    
-        //close browser
-        browser.close();
-        
-    }
+    const browser = await playwright.chromium.launch();
+    //Create a context
+    const context = await browser.newContext()
+    //Create a page
+    const page = await context.newPage()
+
+    //Carry out actions
+    await page.goto('http://whatsmyuseragent.org/');
+    await page.screenshot({path:`output/${playwright.chromium.name()}.png`})
+
+    //close browser
+    browser.close();
 }
 
 main();
