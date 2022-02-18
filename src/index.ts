7799136b24367f551171aa48544005da74404cb4
Kevin <kevin@cbqasolutions.com>
2022-02-18 02:08:30 -0500

    created  fill and click interaction.

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,26 @@ import * as playwright from 'playwright';
 async function main(){
     const {chromium,webkit,firefox} = playwright
     //Launch browser
-    const browser = await playwright.chromium.launch();
+    const browser = await playwright.chromium.launch({
+        headless: false, //open browser
+        slowMo:1500 //define time interaction
+    });
     //Create a context
     const context = await browser.newContext()
     //Create a page
     const page = await context.newPage()
 
     //Carry out actions
-    await page.goto('http://whatsmyuseragent.org/');
+    const url = 'https://playwright.dev/'
+    await page.goto(url);
+
+    //fill search
+    await page.fill('.navbar__search-input','lololo')
+    
+    //click
+    await page.click('.getStarted_1iQB')
     await page.screenshot({path:`output/${playwright.chromium.name()}.png`})
+
 
     //close browser
     browser.close();
